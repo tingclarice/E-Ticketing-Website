47b5faa1c3c51ae5735fcb6a9589af6e540c7089
Clarice <clariceting2006@gmail.com>
2025-03-09 08:45:07 +0700

    added image slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,11 @@
 // trigger autocompletion from Taiwind CSS Intellisense
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // Adjust according to your file structure
+    theme: {
+        extend: {},
+    },
+    plugins: [
+      require('flowbite/plugin'), // Add Flowbite plugin for carousel
+    ],
+};
